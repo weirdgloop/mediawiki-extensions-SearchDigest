597d406bf66fce8cd5f2de0fa5f3b2fb4029d0f0
Jayden Bailey <jaydenkieran@gmail.com>
2019-06-21 16:46:08 +0100

    Switch from using ReverseChronologicalPager to simply using a QueryPage, remove small portion of code used from Wikia's codebase and thus the reference to it in the README file

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ MediaWiki extension which tracks failed searches on your wiki and displays them 
 ## Installation
 
 1. Clone this repository to your MediaWiki installation's `extensions` folder
-2. Modify your `LocalSettings.php` file and add:
+2. Run the `maintenance/update.php` update script to add the necessary tables to your database
+3. Modify your `LocalSettings.php` file and add:
 
 ```php
 // Load the extension
@@ -20,9 +21,10 @@ wfLoadExtension( 'SearchDigest' );
 | Variable | Type | Description | Default |
 | --- | --- | --- | --- |
 | `$wgSearchDigestSafe` | bool | Whether to wrap some extension functionality in try/catch statements to avoid breaking Special:Search if something goes wrong | `true`
+| `$wgSearchDigestStrikeValidPages` | bool | Whether to strike out valid pages (pages that exist) on the special page | `true`
 
 ## Translation
 This extension can be translated through the messages in the `ì18n` folder if you're a developer. As a wiki administrator, you may find it a better option to edit the messages on-site in the MediaWiki namespace.
 
 ## License
-This extension is licensed under GNU GPLv3, [see here](LICENSE) for more information. A small portion of this code is from [Wikia's repository](https://github.com/Wikia/app), licensed under GNU GPLv2.
+This extension is licensed under GNU GPLv3, [see here](LICENSE) for more information.
--- src/SpecialSearchDigest.php
+++ src/SpecialSearchDigest.php
@@ -1,19 +1,77 @@
 <?php
 
-class SpecialSearchDigest extends SpecialPage {
+use MediaWiki\MediaWikiServices;
+
+class SpecialSearchDigest extends QueryPage {
   function __construct() {
     parent::__construct( 'SearchDigest' );
+    $this->linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
   }
 
   function execute( $par ) {
-    $output = $this->getOutput();
-    $output->setPageTitle( wfMessage( 'searchdigest' )->text() );
-    $pager = new SearchDigestPager();
+    $out = $this->getOutput();
+    $out->addWikiText( wfMessage( 'searchdigest-help' )->text() );
+    parent::execute( $par );
+  }
 
-    $html = $pager->getNavigationBar() . $pager->getBody() . $pager->getNavigationBar();
+  function isSyndicated() {
+    return false;
+  }
 
-    $output->addWikiText( wfMessage( 'searchdigest-help' )->text() );
-    $output->addHTML( $html );
+  function getQueryInfo() {
+		// Get the date one week ago
+		$dateLimit = date( 'Y-m-d', ( wfTimestamp( TS_UNIX ) - 604800 ) );
+		return [
+			'tables' => [ 'searchdigest' ],
+			'fields' => [ 'sd_query', 'sd_misses' ],
+			'conds' => [ 'sd_touched > ' . $this->getRecacheDB()->addQuotes( $dateLimit ) ],
+    ];
+  }
+
+
+	public function reallyDoQuery( $limit, $offset = false ) {
+		$fname = static::class . '::reallyDoQuery';
+		$dbr = $this->getRecacheDB();
+		$query = $this->getQueryInfo();
+		$order = $this->getOrderFields();
+		if ( $this->sortDescending() ) {
+			foreach ( $order as &$field ) {
+				$field .= ' DESC';
+			}
+		}
+		$tables = isset( $query['tables'] ) ? (array)$query['tables'] : [];
+		$fields = isset( $query['fields'] ) ? (array)$query['fields'] : [];
+    $conds = isset( $query['conds'] ) ? (array)$query['conds'] : [];
+		$options = isset( $query['options'] ) ? (array)$query['options'] : [];
+		$join_conds = isset( $query['join_conds'] ) ? (array)$query['join_conds'] : [];
+		if ( $limit !== false ) {
+			$options['LIMIT'] = intval( $limit );
+		}
+		if ( $offset !== false ) {
+			$options['OFFSET'] = intval( $offset );
+		}
+    $options['INNER ORDER BY'] = $order;
+    $options['ORDER BY'] = [ 'sd_misses DESC' ];
+    $res = $dbr->select( $tables, $fields, $conds, $fname,
+      $options, $join_conds
+    );
+		return $res;
+  }
+  
+  function getOrderFields() {
+    return [ 'sd_misses' ];
+  }
+  
+  function formatResult( $skin, $row ) {
+		global $wgSearchDigestStrikeValidPages;
+
+		$title = Title::newFromText( $row->sd_query );
+		$link = $this->linkRenderer->makeLink( $title );
+		if ( ( $title->isKnown() === true ) && ( $wgSearchDigestStrikeValidPages === true ) ) {
+			$link = '<s>' . $link . '</s>';
+		}
+
+    return $link . ' (' . $row->sd_misses . ')';
   }
 
   function getGroupName() {
