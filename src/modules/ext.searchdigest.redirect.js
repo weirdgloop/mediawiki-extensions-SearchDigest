7902ad0699ce4177caa861bf2ea1b16f2e827ce5
Jayden Bailey <jaydenkieran@gmail.com>
2024-03-17 22:38:06 +0000

    try and parse urls that are pasted into the redirect script box

--- src/modules/ext.searchdigest.redirect.js
+++ src/modules/ext.searchdigest.redirect.js
@@ -24,7 +24,7 @@ SDRedirectDialog.static.actions = [
   {
     flags: 'safe',
     label: mw.message('cancel').escaped()
-   }
+  }
 ];
 
 SDRedirectDialog.prototype.initialize = function () {
@@ -84,30 +84,49 @@ SDRedirectDialog.prototype.onComboboxInputEnter = function () {
 SDRedirectDialog.prototype.getSetupProcess = function ( data ) {
   data = data || {};
   return SDRedirectDialog.super.prototype.getSetupProcess.call( this, data )
-  .next( function () {
-    this.pageToCreate = data.page;
-    this.$content.find('#sd-ptc').text(data.page);
-    this.actions.setAbilities( {
-      redirect: false
-    } );
-  }, this );
+      .next( function () {
+        this.pageToCreate = data.page;
+        this.$content.find('#sd-ptc').text(data.page);
+        this.actions.setAbilities( {
+          redirect: false
+        } );
+      }, this );
 };
 
 SDRedirectDialog.prototype.getReadyProcess = function ( data ) {
   return SDRedirectDialog.super.prototype.getReadyProcess.call( this, data )
-  .next( function () {
-    this.comboBox.focus();
-  }, this );
+      .next( function () {
+        this.comboBox.focus();
+      }, this );
 }
 
 SDRedirectDialog.prototype.getActionProcess = function ( action ) {
   var self = this;
   if ( action === 'redirect' ) {
     return new OO.ui.Process( function () {
+      let value = this.comboBox.value;
+
+      try {
+        const possiblyAURL = new URL(value)
+        const articlePath = mw.config.get('wgArticlePath').toString().replace('$1', '').replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
+
+        if (
+            possiblyAURL.hostname === window.location.hostname
+        ) {
+          // Given value is a URL where the hostname is the same as the current hostname, so presumably they're
+          // trying to use an external link to a wiki page on the current wiki. Parse it accordingly.
+          const parsedArticle = (possiblyAURL.pathname + possiblyAURL.search).match(new RegExp(articlePath + '(.*)'));
+          if (parsedArticle.length) {
+            value = parsedArticle[1];
+          }
+        }
+      } catch (e) {}
+
+
       api = new mw.Api();
       api.create( this.pageToCreate, {
-        summary: mw.format(SDdata.editsummary, this.comboBox.value)
-      }, SDdata.redirect + " [[" + this.comboBox.value + "]]").done( function(data) {
+        summary: mw.format(SDdata.editsummary, value)
+      }, SDdata.redirect + " [[" + value + "]]").done( function(data) {
         mw.notify( mw.message('searchdigest-redirect-successtext', self.pageToCreate).escaped(), { tag: 'sd-created' } );
         self.close( { page: self.pageToCreate } );
       }).fail( function(data) {
@@ -123,24 +142,24 @@ SDRedirectDialog.prototype.getActionProcess = function ( action ) {
 SDRedirectDialog.prototype.getTeardownProcess = function ( data ) {
   var self = this;
   return SDRedirectDialog.super.prototype.getTeardownProcess.call( this, data )
-  .next( function () {
-    if (data && data.page) {
-      // data.page is the page we created a redirect for
-      // get the span element for the button of the page we just made
-      let btnSpan = $(`*[data-page="${data.page}"]`);
-      // get the a element which is a sibling of the span and then remove the link
-      let pageLink = btnSpan.siblings('a');
-      // remove the link (while keeping the text) and then strike through
-      pageLink.contents().unwrap().wrap('<s></s>');
-    };
+      .next( function () {
+        if (data && data.page) {
+          // data.page is the page we created a redirect for
+          // get the span element for the button of the page we just made
+          let btnSpan = $(`*[data-page="${data.page}"]`);
+          // get the a element which is a sibling of the span and then remove the link
+          let pageLink = btnSpan.siblings('a');
+          // remove the link (while keeping the text) and then strike through
+          pageLink.contents().unwrap().wrap('<s></s>');
+        };
 
-    self.comboBox.setValue('');
-    self.comboBox.setOptions([]);
-    if (self.comboBox.getMenu().isVisible()) {
-      // because of weird clipping bugs
-      self.comboBox.getMenu().toggle();
-    };
-  }, this );
+        self.comboBox.setValue('');
+        self.comboBox.setOptions([]);
+        if (self.comboBox.getMenu().isVisible()) {
+          // because of weird clipping bugs
+          self.comboBox.getMenu().toggle();
+        };
+      }, this );
 }
 
 var redirDialog = new SDRedirectDialog( {
