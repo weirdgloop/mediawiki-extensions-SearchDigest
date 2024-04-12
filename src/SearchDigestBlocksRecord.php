e58bc0c8c003312943cd404a11b96f09ca44caa2
Jayden Bailey <jaydenkieran@gmail.com>
2024-04-12 02:00:03 +0100

    IDatabase alias was removed in 1.41

--- src/SearchDigestBlocksRecord.php
+++ src/SearchDigestBlocksRecord.php
@@ -2,7 +2,7 @@
 
 namespace MediaWiki\Extension\SearchDigest;
 
-use IDatabase;
+use Wikimedia\Rdbms\IDatabase;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\User\ActorNormalization;
 use MediaWiki\User\ActorStore;
