74311b83cb7526c86f33d0b66af0973f8d1cab44
Jayden Bailey <jaydenkieran@gmail.com>
2024-04-18 19:05:59 +0100

    Check for page existence using a LEFT JOIN during the main query

--- src/SpecialSearchDigest.php
+++ src/SpecialSearchDigest.php
@@ -419,12 +419,13 @@ EOD
 		$db = $this->getRecacheDB();
 
 		$conds = [];
+		$joinConds = [];
 
 		if ( $this->par === 'block' ) {
 			$tables = [ 'searchdigest_blocks' ];
 			$fields = [ 'sd_blocks_query', 'sd_blocks_added', 'sd_blocks_actor' ];
 		} else {
-			$tables = [ 'searchdigest' ];
+			$tables = [ 'searchdigest', 'searchdigest_blocks', 'page' ];
 			$fields = [ 'sd_query', 'sd_misses', 'sd_touched' ];
 			$conds = [
 				'sd_touched > ' . $db->addQuotes( date( 'Y-m-d', $this->startTimestamp ) ),
@@ -434,12 +435,36 @@ EOD
 			if ( $this->prefix != '' ) {
 				$conds[] = 'sd_query ' . $db->buildLike( $this->prefix, $db->anyString() );
 			}
+
+			/**
+			 * We're going to do a LEFT JOIN here to check whether articles exist. We could lookup whether each Title
+			 * exists later in a LinkBatch, but that wouldn't let us easily remove results from the result wrapper.
+			 * It is important to note that this does not include any namespace except the main namespace.
+			 */
+			$joinConds['page'] = [
+				'LEFT JOIN', [
+					'page_namespace = 0',
+					'page_title = ' . $db->strreplace( 'sd_query', '" "', '"_"' )
+				]
+			];
+			$conds[] = 'page_title IS NULL';
+
+			/**
+			 * And finally, make sure this query hasn't been blocked by an admin.
+			 */
+			$joinConds['searchdigest_blocks'] = [
+				'LEFT JOIN', [
+					'sd_blocks_query = sd_query'
+				]
+			];
+			$conds[] = 'sd_blocks_query IS NULL';
 		}
 
 		return [
 			'tables' => $tables,
 			'fields' => $fields,
 			'conds' => $conds,
+			'join_conds' => $joinConds
 		];
 	}
 
@@ -531,37 +556,5 @@ EOD
 		}
 
 		return true;
-	}
-
-	protected function preprocessResults( $db, $res ) {
-		if ( !$this->par ) {
-			$queries = [];
-
-			/**
-			 * This pre-processing is similar to QueryPage::executeLBFromResultWrapper(), but slightly different as the
-			 * row is called "sd_query" instead of "title". We're also assuming that the namespace is going to be NS_MAIN,
-			 * but this may not always be true.
-			 */
-			if ( !$res->numRows() ) {
-				return;
-			}
-
-			$batch = $this->getLinkBatchFactory()->newLinkBatch();
-
-			foreach ( $res as $row ) {
-				$batch->add( NS_MAIN, $row->sd_query );
-				$queries[] = $row->sd_query;
-			}
-			$batch->execute();
-
-			$res->seek( 0 );
-
-			/**
-			 * Our list of queries should now be checked to see if any of them are blocked by a user with the
-			 * searchdigest-block right. We're pre-processing this as a batch here, rather than individually,
-			 * for speed and performance (to avoid 500 database calls for each 500 result page).
-			 */
-			$this->blockedQueries = SearchDigestBlocksRecord::checkQueries( $queries );
-		}
 	}
 }
